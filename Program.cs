3b5b1a289c1c30895984b2ff5ae51cff409dd783
unknown <muhammadsyahrul@students.amikom.ac.id.com>
2023-03-20 21:35:23 +0700

    Tugas Class Dan Object

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Taxi
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "dimas";
+            taxi.onDuty = false; taxi.onDuty = true;
+            taxi.numPassanger = 123456789;
+
+
+            taxi.info();
+            taxi.naik();
+            taxi.turun();
+
+            Console.ReadKey();
+        }
+    }
+}
